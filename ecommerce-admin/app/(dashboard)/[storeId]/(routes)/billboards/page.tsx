7bda98f10b2b51389d63040e68805689b1464301
MysticAttribute <damianmorari@gmail.com>
2025-06-20 16:38:12 +0300

    Admin table API

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,13 +1,35 @@
-import { BillboardClient } from "./components/client"
+import { format } from "date-fns";
 
-const BillboardsPage = () => {
+import { BillboardColumn } from "./components/columns"
+import { BillboardClient } from "./components/client"
+import prismadb from "@/lib/prismadb"
+
+const BillboardsPage = async({ params }: { params: Promise<{ storeId: string }>}) => {
+
+    const { storeId } = await params;
+
+    const billboards = await prismadb?.billboard?.findMany({
+        where: {
+            storeId
+        }, 
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formattedBillboards: BillboardColumn[] = billboards.map(item => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, "MMMM do, yyyy"),
+    }));
+
     return (
         <div className="flex-col">
-            <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient />
+            <div className="flex-1 p-8 pt-6 space-y-4">
+                <BillboardClient data={formattedBillboards} />
             </div>
         </div>
     )
 }
 
-export default BillboardsPage
+export default BillboardsPage;
